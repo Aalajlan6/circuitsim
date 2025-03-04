080fd763087ea88468b307ad1131c08cb0aaee96
Aalajlan6 <aalajlan6@gatech.edu>
2025-03-04 15:57:15 -0500

    New attempt with integer square root 2

--- integerSquareRoot.asm
+++ integerSquareRoot.asm
@@ -27,16 +27,21 @@
     AND R3, R3, #0; d = 0
     ADD R3, R3, #3; d = 3
     LD R0, N; R0 = N
-    NOT R4, R0
-    ADD R4, R4, R2
-WHILE BRnz END
-    ADD R2, R2, R3
-    ADD R3, R3, #2
-    ADD R1, R1, #1
+WHILE
+    ADD R4, R2, #0; R4 = a
+    NOT R5, R0    ; R5 = NOT N
+    ADD R4, R4, R5; R4 = R4 + NOT N
+    ADD R4, R4, #1; R4 = a - N
+    BRp END; if a - N > 0, exit loop
+    
+    ;OTHERWISE:
+    ADD R2, R2, R3 ;a = a + d
+    ADD R3, R3, #2 ;d = d + 2
+    ADD R1, R1, #1 ;L = L + 1
     BR WHILE
+    
 END LD R5, RESULT
     STR R1, R5, #0
-    
     HALT
 
 ;; Do not rename or remove any existing labels
