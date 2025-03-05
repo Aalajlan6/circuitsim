8b844521e9a366af450fbbd92d41f66c1df88ae8
Aalajlan6 <aalajlan6@gatech.edu>
2025-03-04 22:49:41 -0500

    Ceaser fix i think

--- caesarCipher.asm
+++ caesarCipher.asm
@@ -9,242 +9,205 @@
 
 .orig x3000
 
+    ; Main: Setup stack and call ENCRYPT
     LD   R6, STACK_PTR
 
-    ; Push SHIFT onto stack
-    LD   R0, SHIFT
+    LD   R0, STRING       ; Push the string address
     ADD  R6, R6, -1
     STR  R0, R6, 0
 
-    ; Push STRING onto stack
-    LD   R0, STRING
+    LD   R0, SHIFT        ; Push the shift value
     ADD  R6, R6, -1
     STR  R0, R6, 0
 
-    ; Call ENCRYPT
-    JSR  ENCRYPT
-
-    ; Cleanup and exit
-    LDR  R0, R6, 0
-    ADD  R6, R6, 3
+    JSR  ENCRYPT          ; Call ENCRYPT
+    
+    LDR  R0, R6, 0        ; Return value (not used here)
+    ADD  R6, R6, 3        ; Pop arguments + return value
     HALT
 
-    ;; Do not rename or remove any existing labels
-    ;; You may change the value of STRING, LENGTH, SHIFT for debugging
-    STACK_PTR     .fill xF000
-    STRING        .fill x4000
-    SHIFT         .fill 7
-    ASCIIUPPERA   .fill 65
-    ASCIILOWERA   .fill 97
-    ALPHABETLEN   .fill 26
+; Data
+STACK_PTR   .fill xF000
+STRING      .fill x4000
+SHIFT       .fill 7
+ASCIIUPPERA .fill 65      ; 'A'
+ASCIILOWERA .fill 97      ; 'a'
+ALPHABETLEN .fill 26      ; 26 letters
 
-; ---------------------------------------------------------
-MOD ;; Do not change this label! Treat this as like the name of the function in a function header
-    ;; Slightly reordered build-up and tear-down, same logic
-    ADD  R6, R6, -1            ; Reserve space for return value
-    ADD  R6, R6, -1            ; Save return address
-    STR  R7, R6, 0
-    ADD  R6, R6, -1            ; Save old frame pointer
-    STR  R5, R6, 0
-    ADD  R6, R6, -1            ; Allocate local variable
-    ADD  R5, R6, 0             ; R5 = new FP
+; ------------------------------------------------------------
+MOD
+    ; MOD(a, b): a mod b
+    ; a is at R5+4, b at R5+5, return in R5+3
 
-    ; Save registers
-    ADD  R6, R6, -1
-    STR  R0, R6, 0
-    ADD  R6, R6, -1
-    STR  R1, R6, 0
-    ADD  R6, R6, -1
-    STR  R2, R6, 0
-    ADD  R6, R6, -1
-    STR  R3, R6, 0
-    ADD  R6, R6, -1
-    STR  R4, R6, 0
+    ; Stack build-up (reordered a bit)
+    ADD  R6, R6, -4
+    STR  R7, R6, 2         ; save RA
+    STR  R5, R6, 1         ; save old FP
+    ADD  R5, R6, 0         ; R5 = new FP
+    ADD  R6, R6, -5        ; push R0-R4
+    STR  R0, R5, -1
+    STR  R1, R5, -2
+    STR  R2, R5, -3
+    STR  R3, R5, -4
+    STR  R4, R5, -5
 
     ; Load arguments
-    LDR  R0, R5, #4   ; a
-    LDR  R1, R5, #5   ; b
+    LDR  R0, R5, 4
+    LDR  R1, R5, 5
 
-WHILE:
-    NOT  R2, R1       ; R2 = -b
-    ADD  R2, R2, #1
-    ADD  R3, R0, R2   ; R3 = a - b
-    BRn  FINISH
-    ADD  R0, R3, #0   ; a = a - b
+WHILE
+    NOT  R2, R1            ; R2 = ~b
+    ADD  R2, R2, #1        ; R2 = -b
+    ADD  R3, R0, R2        ; R3 = a - b
+    BRn  ENDWHILE
+    ADD  R0, R0, R2        ; a = a - b
     BR   WHILE
 
-FINISH
-    STR  R0, R5, #3   ; store return value at FP+3
+ENDWHILE
+    STR  R0, R5, 3         ; store (a mod b)
 
-    ; ------ TEARDOWN ------
-    LDR  R4, R6, #0   ; restore R4
-    ADD  R6, R6, #1
-    LDR  R3, R6, #0
-    ADD  R6, R6, #1
-    LDR  R2, R6, #0
-    ADD  R6, R6, #1
-    LDR  R1, R6, #0
-    ADD  R6, R6, #1
-    LDR  R0, R6, #0
-    ADD  R6, R6, #1
-
-    ADD  R6, R6, #1   ; pop local variable
-
-    LDR  R5, R6, #0   ; old FP
-    ADD  R6, R6, #1
-
-    LDR  R7, R6, #0   ; return address
-    ADD  R6, R6, #1
+    ; Teardown
+    LDR  R4, R5, -5        ; restore R4
+    LDR  R3, R5, -4
+    LDR  R2, R5, -3
+    LDR  R1, R5, -2
+    LDR  R0, R5, -1
+    ADD  R6, R5, #0
+    LDR  R5, R6, 1         ; old FP
+    LDR  R7, R6, 2         ; RA
+    ADD  R6, R6, 3
     RET
 
-; ---------------------------------------------------------
-ENCRYPT ;; Do not change this label! Treat this as like the name of the function in a function header
-    ;; Code your implementation for the ENCRYPT subroutine here, with a slight reorder
+; ------------------------------------------------------------
+ENCRYPT
+    ; ENCRYPT(str, k): modifies str in-place with Caesar shift
 
-    ; ------ BUILDUP ------
-    ADD  R6, R6, -1
-    STR  R7, R6, 0      ; Save return address
-    ADD  R6, R6, -1
-    STR  R5, R6, 0      ; Save old frame pointer
-    ADD  R6, R6, -1     ; Allocate space for a local var
-    ADD  R5, R6, 0      ; R5 = new FP
+    ; Stack build-up
+    ADD  R6, R6, -4
+    STR  R7, R6, 2         ; save RA
+    STR  R5, R6, 1         ; save old FP
+    ADD  R5, R6, 0         ; R5 = new FP
+    ADD  R6, R6, -5        ; push R0-R4
+    STR  R0, R5, -1
+    STR  R1, R5, -2
+    STR  R2, R5, -3
+    STR  R3, R5, -4
+    STR  R4, R5, -5
 
-    ADD  R6, R6, -1     ; Save R4
-    STR  R4, R6, 0
-    ADD  R6, R6, -1     ; Save R3
-    STR  R3, R6, 0
-    ADD  R6, R6, -1     ; Save R2
-    STR  R2, R6, 0
-    ADD  R6, R6, -1     ; Save R1
-    STR  R1, R6, 0
-    ADD  R6, R6, -1     ; Save R0
-    STR  R0, R6, 0
+    ; Get arguments
+    LDR  R0, R5, 4         ; str
+    LDR  R1, R5, 5         ; k
 
-    LDR  R0, R5, #4     ; str
-    LDR  R1, R5, #5     ; k
+    ; length = 0
+    AND  R2, R2, #0
+LENGTH_LOOP
+    ADD  R3, R0, R2
+    LDR  R3, R3, 0
+    BRz  END_LENGTH
+    ADD  R2, R2, #1
+    BR   LENGTH_LOOP
+END_LENGTH
 
-    AND  R3, R3, #0     ; length = 0
+    ; for i in [0..length-1]
+    AND  R3, R3, #0        ; i = 0
+FOR_LOOP
+    NOT  R4, R2            ; R4 = -length
+    ADD  R4, R4, #1
+    ADD  R4, R3, R4        ; R4 = i - length
+    BRzp END_FOR           ; if i >= length, done
 
-WHILE_LOOP:
-    ADD  R4, R0, R3     ; address = &str[length]
-    LDR  R4, R4, #0     ; char = str[length]
-    BRz  FOR_LOOP
-    ADD  R3, R3, #1     ; length++
-    BR   WHILE_LOOP
+    ; Get str[i]
+    ADD  R4, R0, R3
+    LDR  R4, R4, 0
 
-FOR_LOOP:
-    AND  R2, R2, #0     ; i = 0
-
-FOR_LOOP_CHECK:
-    NOT  R4, R3
-    ADD  R4, R4, #1     ; (i - length) compare
-    ADD  R4, R2, R4
-    BRzp AFTER_FOR
-
-    ADD  R4, R0, R2     ; address of str[i]
-    LDR  R4, R4, #0     ; R4 = str[i]
-
-IF_STATMENT:
-    LD   R7, ASCIILOWERA
-    NOT  R7, R7
-    ADD  R7, R7, #1     ; R7 = -'a'
-    ADD  R7, R4, R7     ; char - 'a'
-    BRn  ELSE_IF
-
-    LD   R7, ASCIILOWERA
-    ADD  R7, R7, #10
-    ADD  R7, R7, #15    ; 'z'
-    NOT  R7, R7
-    ADD  R7, R7, #1     ; -char
-    ADD  R7, R4, R7     ; 'z' - char
-    BRp  ELSE_IF
-
+    ; Check if 'a' <= char <= 'z'
     LD   R7, ASCIILOWERA
     NOT  R7, R7
     ADD  R7, R7, #1
-    ADD  R4, R4, R7     ; char -= 'a'
-    ADD  R4, R4, R1     ; char += k
+    ADD  R7, R4, R7        ; char - 'a'
+    BRn  CHECK_UPPER
 
-    LD   R7, ALPHABETLEN
-    ADD  R6, R6, -1
-    STR  R7, R6, #0     ; push second arg
-    ADD  R6, R6, -1
-    STR  R4, R6, #0     ; push first arg
-    JSR  MOD
-
-    LDR  R4, R6, #0     ; R4 = result
-    ADD  R6, R6, #3
-
-    LD   R7, ASCIILOWERA
-    ADD  R4, R4, R7
-    BR   STORING_CHAR
-
-ELSE_IF:
-    LD   R7, ASCIIUPPERA
-    NOT  R7, R7
-    ADD  R7, R7, #1
-    ADD  R7, R4, R7     ; char - 'A'
-    BRn  STORING_CHAR
-
-    LD   R7, ASCIIUPPERA
-    ADD  R7, R7, #10
+    LD   R7, ASCIILOWERA   ; compute 'z'
     ADD  R7, R7, #15
+    ADD  R7, R7, #10
     NOT  R7, R7
     ADD  R7, R7, #1
-    ADD  R7, R4, R7     ; 'Z' - char
-    BRp  STORING_CHAR
+    ADD  R7, R4, R7        ; char - 'z'
+    BRp  CHECK_UPPER
 
+    ; Lowercase transform
+    LD   R7, ASCIILOWERA
+    NOT  R7, R7
+    ADD  R7, R7, #1
+    ADD  R4, R4, R7        ; char -= 'a'
+    ADD  R4, R4, R1        ; char += k
+    LD   R7, ALPHABETLEN
+    ADD  R6, R6, -1
+    STR  R7, R6, 0
+    ADD  R6, R6, -1
+    STR  R4, R6, 0
+    JSR  MOD
+    LDR  R4, R6, 0
+    ADD  R6, R6, 2
+    LD   R7, ASCIILOWERA
+    ADD  R4, R4, R7
+    BR   WRITE_CHAR
+
+CHECK_UPPER
+    ; Check if 'A' <= char <= 'Z'
     LD   R7, ASCIIUPPERA
     NOT  R7, R7
     ADD  R7, R7, #1
-    ADD  R4, R4, R7     ; char -= 'A'
-    ADD  R4, R4, R1     ; char += k
+    ADD  R7, R4, R7
+    BRn  WRITE_CHAR
 
+    LD   R7, ASCIIUPPERA
+    ADD  R7, R7, #15
+    ADD  R7, R7, #10
+    NOT  R7, R7
+    ADD  R7, R7, #1
+    ADD  R7, R4, R7        ; char - 'Z'
+    BRp  WRITE_CHAR
+
+    ; Uppercase transform
+    LD   R7, ASCIIUPPERA
+    NOT  R7, R7
+    ADD  R7, R7, #1
+    ADD  R4, R4, R7        ; char -= 'A'
+    ADD  R4, R4, R1        ; char += k
     LD   R7, ALPHABETLEN
     ADD  R6, R6, -1
-    STR  R7, R6, #0
+    STR  R7, R6, 0
     ADD  R6, R6, -1
-    STR  R4, R6, #0
+    STR  R4, R6, 0
     JSR  MOD
-    LDR  R4, R6, #0
-    ADD  R6, R6, #3
-
+    LDR  R4, R6, 0
+    ADD  R6, R6, 2
     LD   R7, ASCIIUPPERA
     ADD  R4, R4, R7
 
-STORING_CHAR:
-    ADD  R7, R0, R2
-    STR  R4, R7, #0
-    ADD  R2, R2, #1     ; i++
-    BR   FOR_LOOP_CHECK
+WRITE_CHAR
+    ; Save new char
+    ADD  R7, R0, R3
+    STR  R4, R7, 0
+    ADD  R3, R3, #1
+    BR   FOR_LOOP
 
-AFTER_FOR:
-    STR  R0, R5, #3     ; if we want to store a return val
-
-    ; ------ TEARDOWN ------
-    LDR  R4, R6, #0
-    ADD  R6, R6, #1
-    LDR  R3, R6, #0
-    ADD  R6, R6, #1
-    LDR  R2, R6, #0
-    ADD  R6, R6, #1
-    LDR  R1, R6, #0
-    ADD  R6, R6, #1
-    LDR  R0, R6, #0
-    ADD  R6, R6, #1
-
-    ADD  R6, R6, #1     ; pop local var
-
-    LDR  R5, R6, #0
-    ADD  R6, R6, #1
-
-    LDR  R7, R6, #0
-    ADD  R6, R6, #1
+END_FOR
+    ; Teardown
+    LDR  R4, R5, -5
+    LDR  R3, R5, -4
+    LDR  R2, R5, -3
+    LDR  R1, R5, -2
+    LDR  R0, R5, -1
+    ADD  R6, R5, 0
+    LDR  R5, R6, 1
+    LDR  R7, R6, 2
+    ADD  R6, R6, 3
     RET
 
 .end
 
-;; You may change the value of the string for debugging
 .orig x4000
     .stringz "hello"
 .end
