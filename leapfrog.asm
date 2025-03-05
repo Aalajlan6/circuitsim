2883d5331079e4783d3039e614258e8e7b82f335
Aalajlan6 <aalajlan6@gatech.edu>
2025-03-04 22:22:51 -0500

    Find min fix omar

--- leapfrog.asm
+++ leapfrog.asm
@@ -6,27 +6,40 @@
 ;;=============================================================
 
 .orig x3000
-;; Suggested Pseudocode (see PDF for explanation)
-;;
-;; inc = x0100
-;; func_addr = starting address of function
-;; copy_addr = func_addr + inc
-;; start_copy = copy_addr
-;; stop_addr = xFE00  
-;; if (start_copy - stop_addr == 0):
-;;      HALT
-;; curr = mem[func_addr]
-;; while (curr != 0) {
-;;      curr = mem[func_addr]
-;;      mem[copy_addr] = curr
-;;      func_addr++
-;;      copy_addr++
-;; }
-;; mem[copy_addr] = 0
-;; PC = start_copy
-;; .fill 0
+SETUP
+    LD R3, VALUE     ; R3 = x0100
+    LEA R2, SETUP    ; R2 = address
+    ADD R4, R2, R3   ; R4 = R2 + R3
 
-;; YOUR CODE HERE
+    LD R5, STOP      ; R5 = xFE00
+    ADD R1, R4, #0   ; R1 = R4
 
-        .fill 0
+    NOT R6, R5
+    ADD R6, R6, #1
+    ADD R6, R1, R6
+    BRnp SKIP
+    HALT
+
+SKIP
+    AND R0, R0, #0 
+
+LOOP
+    LDR R7, R2, #0
+    BRz DONE
+
+    STR R7, R4, #0
+    ADD R2, R2, #1
+    ADD R4, R4, #1
+    BR LOOP
+
+DONE
+    AND R7, R7, #0
+    STR R7, R4, #0
+    JMP R1
+
+VALUE   .FILL x0100
+PLACE   .FILL x3000
+STOP    .FILL xFE00
+        .FILL x0000
+
 .end
