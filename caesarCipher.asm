eb62f18ef92d25e611e5ce83d04da817f0e72224
Aalajlan6 <aalajlan6@gatech.edu>
2025-03-04 22:29:14 -0500

    Find min fix omar

--- caesarCipher.asm
+++ caesarCipher.asm
@@ -12,11 +12,11 @@
     LD R6, STACK_PTR
     
     ADD R6, R6, -1
-    LD R0, STRING
+    LD R0, SHIFT
     STR R0, R6, 0
     
     ADD R6, R6, -1
-    LD R0, SHIFT
+    LD R0, STRING
     STR R0, R6, 0
     
     JSR ENCRYPT 
@@ -26,7 +26,7 @@
 
 STACK_PTR .fill xF000
 STRING    .fill x4000
-SHIFT     .fill 5
+SHIFT     .fill 7
 ALPHA     .fill 26
 
 MOD  
@@ -35,17 +35,21 @@ MOD
     ADD R6, R6, -1  
     STR R5, R6, 0  
     
-    LDR R0, R6, #4
-    LDR R1, R6, #5 
-    
-LOOP:
-    ADD R3, R0, R1  
-    BRzp END  
-    ADD R0, R0, #-1  
-    BR LOOP  
+    LDR R0, R5, 4  
+    LDR R1, R5, 5  
 
-END:  
-    STR R0, R5, #3  
+WHILE:
+    NOT R2, R1  
+    ADD R2, R2, 1  
+    ADD R3, R0, R2  
+    BRn FINISH  
+    ADD R0, R3, #0  
+    BR WHILE  
+
+FINISH:  
+    STR R0, R5, 3  
+    ADD R6, R6, 1  
+    LDR R5, R6, 0  
     ADD R6, R6, 1  
     LDR R7, R6, 0  
     ADD R6, R6, 1  
@@ -55,16 +59,27 @@ ENCRYPT
     ADD R6, R6, -1  
     STR R7, R6, 0  
     
-    LDR R0, R6, #4  
-    LDR R1, R6, #5  
+    LDR R0, R5, 4  
+    LDR R1, R5, 5  
     
-LENGTH:  
-    ADD R3, R0, #0  
-    BRz DONE  
+FIND_LEN:  
+    ADD R2, R0, #0  
+    LDR R2, R2, #0  
+    BRz FOR_LOOP  
     ADD R0, R0, #1  
-    BR LENGTH  
+    BR FIND_LEN  
     
-DONE:
+FOR_LOOP:
+    ADD R2, R0, #0  
+    LDR R3, R2, #0  
+    BRz END_LOOP  
+    ADD R3, R3, R1  
+    JSR MOD  
+    STR R3, R2, #0  
+    ADD R0, R0, #1  
+    BR FOR_LOOP  
+
+END_LOOP:
     LDR R7, R6, 0  
     ADD R6, R6, 1  
     RET  
